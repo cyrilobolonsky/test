60ac29aa7e86519a1e2e1780ca449f74223ac45c
arendt <soc.net@seznam.cz>
2024-03-09 16:38:16 +0100

    first commit: committing all files

---
+++ ex4.py
@@ -0,0 +1,42 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print(x)
+print(y)
+
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+print(joke_evaluation.format(hilarious))
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+print("Mary had a little lamb.")
+print("Its fleece iswhite as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end ='' )
+print(end7 + end8 + end9 + end10 + end11 + end12)
